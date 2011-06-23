fed208fdef9ceb9ed07b79fc5ac6130b21329612
Peter Wong <peter.wm.wong@gmail.com>
2011-06-23 01:46:05 -0500

    Don't hot link ie gtfo image

--- cells/IEGTFO.coffee
+++ cells/IEGTFO.coffee
@@ -3,7 +3,7 @@ define
     """
     Game Over
     <br>
-    <img src='http://9.media.collegehumor.cvcdn.com/2/6/collegehumor.7dea5f12f61024453fe144d6e08fed68.jpg'>
+    <img src='cells/IEGTFO.jpg'>
     <br>
     My condolences to this <a href='http://blogs.msdn.com/b/ie/archive/2011/04/27/ie9-compat-inspector.aspx'>poor</a> <a href='http://www.viddler.com/explore/ie8videos/videos/40'>sap</a>.
     """
--- cells/IEGTFO.js
+++ cells/IEGTFO.js
@@ -1,5 +1,5 @@
 define({
   render: function() {
-    return "Game Over\n<br>\n<img src='http://9.media.collegehumor.cvcdn.com/2/6/collegehumor.7dea5f12f61024453fe144d6e08fed68.jpg'>\n<br>\nMy condolences to this <a href='http://blogs.msdn.com/b/ie/archive/2011/04/27/ie9-compat-inspector.aspx'>poor</a> <a href='http://www.viddler.com/explore/ie8videos/videos/40'>sap</a>.";
+    return "Game Over\n<br>\n<img src='cells/IEGTFO.jpg'>\n<br>\nMy condolences to this <a href='http://blogs.msdn.com/b/ie/archive/2011/04/27/ie9-compat-inspector.aspx'>poor</a> <a href='http://www.viddler.com/explore/ie8videos/videos/40'>sap</a>.";
   }
 });
